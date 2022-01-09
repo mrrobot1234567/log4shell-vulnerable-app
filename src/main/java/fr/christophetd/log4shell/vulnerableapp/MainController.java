6de7b7c147df73235bce90c06eaf398d91005501
mrrobot1234567 <97400290+mrrobot1234567@users.noreply.github.com>
2022-01-09 22:03:16 +0530

    Update MainController.java

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -18,5 +18,11 @@ public class MainController {
         logger.info("Received a request for API version " + apiVersion);
         return "Hello, world!";
     }
+    
+    @GetMapping("/api")
+    public String index(@RequestHeader("User-Agent") String apiVersion) {
+        logger.info("Received a request for API version " + apiVersion);
+        return "Something went wrong!";
+    }
 
 }
